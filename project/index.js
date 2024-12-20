4f5c1d7f13cfb3e339b3361f928c209fdd786698
bhavani shanker <bhavanishanker@bhavanis-MacBook-Pro.local>
2024-12-20 16:08:46 +0530

    done1

--- project/index.js
+++ project/index.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/", roomAnalyticsRouter);
-
+app.get("/",(req,res)=>{
+  res.status(200).send("Hello")
+})
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
